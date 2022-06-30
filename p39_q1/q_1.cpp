a5246570955278c3b110d06ed73380a2f46baf1a
[gominzip] <[ahaminji@naver.com]>
2022-07-01 00:13:46 +0900

    [삭제] 필요 없는 자료

--- p39_q1/q_1.cpp
+++
@@ -1,8 +0,0 @@
-//이름출력프로그램, print 함수는 한 번만 호출
-#include <stdio.h> // 헤더파일 선언
-
-int main(void) // main 함수의 시작
-{
-	printf("고민지\n고 민 지\n고  민  지"); // 문자열 출력, \n으로 개행
-	return 0; // 0 반환
-} // main 함수의 끝
